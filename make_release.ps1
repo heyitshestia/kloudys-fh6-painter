bf8f3e931cf59f6fa823ff2d8429c711b77417f9
heyitshestia <78128225+heyitshestia@users.noreply.github.com>
2026-05-25 14:59:57 +0000

    Revert "Update bundled generator"
    This reverts commit 6e8ade13032e38484bb956c26ac0d43f459ff516.

--- make_release.ps1
+++ make_release.ps1
@@ -36,7 +36,6 @@ $ProjectItems = @(
     "launcher_qt.py",
     "generator_backend.py",
     "forza_generator_v2.py",
-    "kloudys-fh6-generator.exe",
     "forza-painter-geometrize-go.exe",
     "geometry_json.py",
     "game_profiles.py",
@@ -137,7 +136,6 @@ try {
         "KloudysFH6Painter/03_update_from_github.bat",
         "KloudysFH6Painter/app_qt.py",
         "KloudysFH6Painter/forza_generator_v2.py",
-        "KloudysFH6Painter/kloudys-fh6-generator.exe",
         "KloudysFH6Painter/settings/a.fast-ugly.ini"
     )
     foreach ($RequiredName in $Required) {
